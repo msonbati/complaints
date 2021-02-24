90b83f2143986bd9929fcf5a42642900c29d6a8a
mahmoud.elsonbati <mahmoud.elsonbati@gmail.com>
2021-02-24 11:35:39 +0100

    added storage properties

--- src/main/java/com/quality/complaints/ComplaintsApplication.java
+++ src/main/java/com/quality/complaints/ComplaintsApplication.java
@@ -1,9 +1,12 @@
 package com.quality.complaints;
 
+import com.quality.complaints.config.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class ComplaintsApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/quality/complaints/config/StorageProperties.java
@@ -0,0 +1,22 @@
+package com.quality.complaints.config;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("storage")
+public class StorageProperties {
+
+    /**
+     * Folder location for storing files
+     */
+    private String location = "src/main/upload-dir";
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+}
